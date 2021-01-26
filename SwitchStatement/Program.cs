aa468f0c1e6bf1da9ac323392fb65e833138a0f0
Mohammed Hossain <jamalhossain1629@gmail.com>
2021-01-26 10:27:17 -0500

    done

---
+++ SwitchStatement/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SwitchStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type your favorite subjct");
+
+            string favoriteSubject = Console.ReadLine();
+
+            switch (favoriteSubject)
+            {
+                case "MO":
+                    Console.WriteLine("I love Math");
+                    break;
+                case "EN":
+                    Console.WriteLine("I love English");
+                    break;
+                default:
+                    Console.WriteLine("Nothing");
+                    break;
+            }
+
+        }
+    }
+}
